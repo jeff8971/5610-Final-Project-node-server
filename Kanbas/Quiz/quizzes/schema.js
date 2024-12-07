21b865b1a82d463cd9f101bfc45d81c16c6077ce
jeff8971 <zhao.yuan2@northeastern.edu>
2024-12-07 17:54:20 -0500

    modify quiz schema

--- Kanbas/Quiz/quizzes/schema.js
+++ Kanbas/Quiz/quizzes/schema.js
@@ -5,14 +5,27 @@ const quizSchema = new mongoose.Schema({
     title: String,
     description: String,
     publish: Boolean,
+    multipleAttempts: Boolean,
     attempts: Number,
+    assignmentGroup: String,
+    quizType: {
+      type: String,
+      enum: ["Graded Quiz", "Practice Quiz", "Graded Survey", "Ungraded Survey"],
+      default: "Graded Quiz",
+    },
+    AssignmentGroup:{
+      type: String,
+      enum: ["Quizzes", "Exams", "Assignments", "Project"],
+      default: "Quizzes", 
+    },
     availableDate: String,
     availableUntilDate: String,
     dueDate: String,
     points: Number,
     numberOfQuestions: Number,
     timeLimit: Number,
-    courseId: String
+    courseId: String,
+    shuffleAnswers: Boolean,
   },
   { collection: "quizzes" }
 );
